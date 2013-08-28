c1c1b411cbd28baf151e39f713cacd63a557502f
Dave Shah <david.a.shah@gmail.com>
2013-08-28 13:59:22 -0400

    Update Microsoft.PowerShell_profile.ps1

--- windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 #Aliases
-new-item alias:build -value 'C:\WINDOWS\Microsoft .NET\Framework\v3.5\MSBuild.exe'
+new-item alias:build -value 'C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe'
 new-item alias:sublime -value 'C:\Program Files\Sublime Text 2\sublime_text.exe'
 new-item alias:git -value 'C:\Program Files\Git\bin\git.exe'
