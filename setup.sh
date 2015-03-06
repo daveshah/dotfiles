851be788dc5ee41413390e6057c7ce72d447944b
David Shah <david.a.shah@gmail.com>
2015-03-06 13:54:01 -0500

    Add setup for Vundle

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VUNDLE_FILE=~/.vim/bundle/Vundle.vim
+
+
+#Install Vundle if not available
+if [ ! -d "$VUNDLE_FILE" ]; then
+	echo "Vundle not found. Installing Vundle"
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
