32b93b52aec251b117711f0e96e01aae13964180
David Shah <david.a.shah@gmail.com>
2015-03-06 14:03:11 -0500

    add vimrc

--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@
 
 VUNDLE_FILE=~/.vim/bundle/Vundle.vim
 
-
 #Install Vundle if not available
 if [ ! -d "$VUNDLE_FILE" ]; then
 	echo "Vundle not found. Installing Vundle"
 	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ ! -f ~/.vimrc ]; then
+	echo "Updating your .vimrc"
+	ln -s .dotfiles/vim/.vimrc ~/.vimrc
 fi
 
